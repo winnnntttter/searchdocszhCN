d8e47d5ea613560dd5cc55a8785606fc14a49178
hanwm <hanwm@chsi.com.cn>
2019-12-24 17:26:36 +0800

    0.0.1

---
+++ README.md
@@ -0,0 +1,3 @@
+# searchdocszhCN
+通过stack overflow和MDN搜索，快捷键分别为`ctrl+alt+shift+s` 和 `ctrl+alt+shift+m`，命令为`searchdocszhCN.searchSO`和`searchdocszhCN.searchMDN`,感谢roblourens和他的searchdocs。
+Thanks to roblourens and his/her searchdocs
---
+++ extension.js
@@ -0,0 +1,57 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require("vscode");
+const open = require("open");
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+  vscode.commands.registerCommand("searchdocszhCN.searchSO", function() {
+    getQueryAndSearch("http://stackoverflow.com/search?q=%s", "Stack Overflow");
+  });
+  vscode.commands.registerCommand("searchdocszhCN.searchMDN", function() {
+    getQueryAndSearch("https://developer.mozilla.org/zh-CN/search?q=%s", "MDN");
+  });
+  function getQueryAndSearch(searchUrl, providerName) {
+    getQuery(searchUrl, providerName).then(function(searchText) {
+      if (searchText) {
+        open(searchUrl.replace("%s", encodeURIComponent(searchText)));
+      }
+    });
+  }
+  function getQuery(searchUrl, providerName) {
+    var editor = vscode.window.activeTextEditor;
+    if (!editor) return Promise.resolve(null);
+    var searchWordUnderCursor = vscode.workspace.getConfiguration("searchdocs")["searchWordUnderCursor"];
+    var selection = editor.selection;
+    if (searchWordUnderCursor || !selection.isEmpty) {
+      var searchText = getSelectedTextOrCursorWord(editor).trim();
+      if (searchText) return Promise.resolve(searchText.trim());
+    }
+    return vscode.window.showInputBox({ prompt: providerName + ": 请输入" }).then(function(searchText) {
+      return searchText && searchText.trim();
+    });
+  }
+  function getSelectedTextOrCursorWord(editor) {
+    var selection = editor.selection;
+    var doc = editor.document;
+    if (selection.isEmpty) {
+      var cursorWordRange = doc.getWordRangeAtPosition(selection.active);
+      return cursorWordRange ? doc.getText(cursorWordRange) : "";
+    } else {
+      return doc.getText(selection);
+    }
+  }
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate
+};
